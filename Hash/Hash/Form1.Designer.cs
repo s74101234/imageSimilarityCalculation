73e79414195602429192dd8b55f01836bb4a2f74
Kuo Yu-Cheng <s74101234@gmail.com>
2017-10-14 18:18:16 +0800

    hash upload
    hash upload

---
+++ Hash/Hash/Form1.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace Hash
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(13, 13);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(130, 116);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(149, 13);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(123, 116);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(13, 136);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(117, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "主要圖片";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(149, 135);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(110, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "次要圖片";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(13, 166);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(117, 23);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Hash計算";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 238);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(30, 12);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "result";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(149, 165);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(110, 23);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "加入Canny計算";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(224, 200);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(35, 22);
+            this.textBox1.TabIndex = 7;
+            this.textBox1.Text = "70";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(149, 203);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(69, 12);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Canny數值 : ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Hash/Hash/Form1.cs
@@ -0,0 +1,238 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using Emgu.Util;
+using Emgu.CV;
+using Emgu.CV.CvEnum;
+using Emgu.CV.Features2D;
+using Emgu.CV.Structure;
+using Emgu.CV.Util;
+using Emgu.CV.UI;
+
+
+
+namespace Hash
+{
+    public partial class Form1 : Form
+    {
+
+        Image<Bgr, Byte> imgResult1;
+        Image<Bgr, Byte> imgResult2;
+        Image<Gray, Byte> imgCannyResult1;
+        Image<Gray, Byte> imgCannyResult2;
+        String imgresult1Src;
+        String imgresult2Src;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog Openfile = new OpenFileDialog();
+            if (Openfile.ShowDialog() == DialogResult.OK)
+            {
+                imgresult1Src = Openfile.FileName;
+
+                Image<Bgr, Byte> image1;
+                image1 = new Image<Bgr, Byte>(Openfile.FileName);
+
+
+                ////預設70
+                //double cannyValue = Convert.ToDouble(textBox1.Text);
+                //Image<Gray, Byte> grayImage = image1.Convert<Gray, Byte>();
+                //Image<Gray, Byte> cannyGray = grayImage.Canny(cannyValue, cannyValue);
+                ////Image<Gray, Byte> notImage = cannyGray.Not();
+                //imgCannyResult1 = cannyGray;
+
+
+                imgResult1 = image1;
+
+                pictureBox1.Image = image1.ToBitmap();
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog Openfile = new OpenFileDialog();
+            if (Openfile.ShowDialog() == DialogResult.OK)
+            {
+                imgresult2Src = Openfile.FileName;
+
+                Image<Bgr, Byte> image2;
+                image2 = new Image<Bgr, Byte>(Openfile.FileName);
+
+
+                ////預設70
+                //double cannyValue = Convert.ToDouble(textBox1.Text);
+                //Image<Gray, Byte> grayImage = image2.Convert<Gray, Byte>();
+                //Image<Gray, Byte> cannyGray = grayImage.Canny(cannyValue, cannyValue);
+                ////Image<Gray, Byte> notImage = cannyGray.Not();
+                //imgCannyResult2 = cannyGray;
+
+
+                imgResult2 = image2;
+
+                pictureBox2.Image = image2.ToBitmap();
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            getResult(imgResult1.ToBitmap(), imgResult2.ToBitmap());
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            //預設70
+            double cannyValue = Convert.ToDouble(textBox1.Text);
+
+            //img 1 
+            Image<Gray, Byte> grayImage1 = imgResult1.Convert<Gray, Byte>();
+            Image<Gray, Byte> cannyGray1 = grayImage1.Canny(cannyValue, cannyValue);
+            
+            imgCannyResult1 = cannyGray1;
+
+            //img 2 
+            Image<Gray, Byte> grayImage2 = imgResult2.Convert<Gray, Byte>();
+            Image<Gray, Byte> cannyGray2 = grayImage2.Canny(cannyValue, cannyValue);
+
+            imgCannyResult2 = cannyGray2;
+
+
+            getResult(imgCannyResult1.ToBitmap(), imgCannyResult2.ToBitmap());
+        }
+
+        private void getResult(Image imgresult1, Image imgresult2)
+        {
+            //第五步：利用 哈希 計算 Hammingdistance 取得百分比
+
+            label1.Text = compareHash(GetHash(imgresult1), GetHash(imgresult2)).ToString() + "%";
+
+        }
+
+        private double compareHash(int[] first , int[] second)
+        {
+            //first
+            StringBuilder hashCode1 = new StringBuilder();
+            for (int i = 0; i < first.Length; i += 4)
+            {
+                int firstResult = first[i] * (int)Math.Pow(2, 3) + first[i + 1] * (int)Math.Pow(2, 2) + first[i + 2] * (int)Math.Pow(2, 1) + first[i + 2];
+                hashCode1.Append(string.Format("{0:x}", Convert.ToInt32(firstResult)));//二进制转为16进制  
+            }
+            String sourceHashCode1 = hashCode1.ToString();
+
+            //second
+            StringBuilder hashCode2 = new StringBuilder();
+            for (int i = 0; i < second.Length; i += 4)
+            {
+                int secondResult = second[i] * (int)Math.Pow(2, 3) + second[i + 1] * (int)Math.Pow(2, 2) + second[i + 2] * (int)Math.Pow(2, 1) + second[i + 2];
+                hashCode2.Append(string.Format("{0:x}", Convert.ToInt32(secondResult)));//二进制转为16进制  
+            }
+            String sourceHashCode2 = hashCode2.ToString();
+
+            //Hammingdistance
+            int difference = 0;
+            int length = sourceHashCode1.Length;
+
+            for (int i = 0; i < length; i++)
+            {
+                if (sourceHashCode1[i] != sourceHashCode2[i])
+                {
+                    difference++;
+                }
+            }
+
+            //0~16轉百分比
+            double result;
+            result = 100 - (difference * (100 / 16));
+
+            return result;
+        }
+
+        public int[] GetHash(Image imgresult1)
+        {
+            int width = 8;
+            int height = 8;
+
+            //第一步：縮小尺寸
+
+            Image SourceImg = ReduceSize(imgresult1, width, height);
+
+            //第二步：簡化色彩
+
+            Byte[] pixels = ReduceColor(SourceImg);
+
+            //第三步：計算平均
+
+            Byte average = CalcAverage(pixels);
+
+            //第四步：比較像素的灰度
+            int[] result = ComputeBits(pixels, average);
+
+
+            return result;
+        }
+
+        private Image ReduceSize(Image SourceImg, int width = 8, int height = 8)
+        {
+            Image image = SourceImg.GetThumbnailImage(width, height, () => { return false; }, IntPtr.Zero);
+            return image;
+        }
+
+        private Byte[] ReduceColor(Image image)
+        {
+            Bitmap bitMap = new Bitmap(image);
+            Byte[] grayValues = new Byte[image.Width * image.Height];
+
+            for (int x = 0; x < image.Width; x++)
+                for (int y = 0; y < image.Height; y++)
+                {
+                    Color color = bitMap.GetPixel(x, y);
+                    byte grayValue = (byte)((color.R * 30 + color.G * 59 + color.B * 11) / 100);
+                    grayValues[x * image.Width + y] = grayValue;
+                }
+            return grayValues;
+        }
+
+        private Byte CalcAverage(byte[] values)
+        {
+            int sum = 0;
+            for (int i = 0; i < values.Length; i++)
+                sum += (int)values[i];
+            return Convert.ToByte(sum / values.Length);
+        }
+
+        private int[] ComputeBits(byte[] values, byte averageValue)
+        {
+
+            int[] result = new int[values.Length];
+
+            for (int i = 0; i < values.Length; i++)
+            {
+                if (values[i] < averageValue)
+                    result[i] = 0;
+                else
+                    result[i] = 1;
+            }
+
+            return result;
+        }
+
+        
+    }
+}
