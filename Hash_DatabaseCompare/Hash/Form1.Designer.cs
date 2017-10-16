22db459d7f06af2e4c490533e89f9620d42ddfb1
Kuo Yu-Cheng <s74101234@gmail.com>
2017-10-16 11:33:22 +0800

    upload Hash_DatabaseCompare
    upload Hash_DatabaseCompare

---
+++ Hash_DatabaseCompare/Hash/Form1.Designer.cs
@@ -0,0 +1,258 @@
+﻿namespace Hash
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button5 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.button6 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(13, 13);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(130, 116);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(149, 13);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(123, 116);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(13, 136);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(117, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "主要圖片";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(149, 135);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(110, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "次要圖片";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(13, 166);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(117, 23);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Hash計算";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 259);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(30, 12);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "result";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(149, 165);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(110, 23);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "加入Canny計算";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(224, 200);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(35, 22);
+            this.textBox1.TabIndex = 7;
+            this.textBox1.Text = "70";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(149, 203);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(69, 12);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Canny數值 : ";
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(149, 229);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(69, 23);
+            this.button5.TabIndex = 9;
+            this.button5.Text = "資料處理";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(11, 319);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(44, 12);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "database";
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(225, 235);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(83, 16);
+            this.radioButton1.TabIndex = 11;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "新增資料庫";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(225, 257);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(83, 16);
+            this.radioButton2.TabIndex = 12;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "更新資料庫";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(225, 279);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(83, 16);
+            this.radioButton3.TabIndex = 13;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "讀取資料庫";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Location = new System.Drawing.Point(225, 301);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(83, 16);
+            this.radioButton4.TabIndex = 14;
+            this.radioButton4.TabStop = true;
+            this.radioButton4.Text = "刪除資料庫";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(332, 229);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(116, 23);
+            this.button6.TabIndex = 15;
+            this.button6.Text = "資料庫圖片計算";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(473, 453);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.radioButton4);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.Button button6;
+    }
+}
+
---
+++ Hash_DatabaseCompare/Hash/Form1.cs
@@ -0,0 +1,466 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using Emgu.Util;
+using Emgu.CV;
+using Emgu.CV.CvEnum;
+using Emgu.CV.Features2D;
+using Emgu.CV.Structure;
+using Emgu.CV.Util;
+using Emgu.CV.UI;
+
+using System.Data.SqlClient;
+
+
+
+namespace Hash
+{
+    public partial class Form1 : Form
+    {
+
+        Image<Bgr, Byte> imgResult1;
+        Image<Bgr, Byte> imgResult2;
+        Image<Gray, Byte> imgCannyResult1;
+        Image<Gray, Byte> imgCannyResult2;
+        String imgresult1Src;
+        String imgresult2Src;
+
+        string strDbCon = @"Data Source = (LocalDB)\MSSQLLocalDB; AttachDbFilename = " +
+                            @"C:\VusialStudio\Hash_資料庫建立\Hash\pictureData.mdf" +
+                            "; Integrated Security = True";
+
+        string[] files = Directory.GetFileSystemEntries("c:\\圖片庫");
+        string[] filename;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog Openfile = new OpenFileDialog();
+            if (Openfile.ShowDialog() == DialogResult.OK)
+            {
+                imgresult1Src = Openfile.FileName;
+
+                Image<Bgr, Byte> image1;
+                image1 = new Image<Bgr, Byte>(Openfile.FileName);
+
+
+                ////預設70
+                //double cannyValue = Convert.ToDouble(textBox1.Text);
+                //Image<Gray, Byte> grayImage = image1.Convert<Gray, Byte>();
+                //Image<Gray, Byte> cannyGray = grayImage.Canny(cannyValue, cannyValue);
+                ////Image<Gray, Byte> notImage = cannyGray.Not();
+                //imgCannyResult1 = cannyGray;
+
+
+                imgResult1 = image1;
+
+                pictureBox1.Image = image1.ToBitmap();
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog Openfile = new OpenFileDialog();
+            if (Openfile.ShowDialog() == DialogResult.OK)
+            {
+                imgresult2Src = Openfile.FileName;
+
+                Image<Bgr, Byte> image2;
+                image2 = new Image<Bgr, Byte>(Openfile.FileName);
+
+
+                ////預設70
+                //double cannyValue = Convert.ToDouble(textBox1.Text);
+                //Image<Gray, Byte> grayImage = image2.Convert<Gray, Byte>();
+                //Image<Gray, Byte> cannyGray = grayImage.Canny(cannyValue, cannyValue);
+                ////Image<Gray, Byte> notImage = cannyGray.Not();
+                //imgCannyResult2 = cannyGray;
+
+
+                imgResult2 = image2;
+
+                pictureBox2.Image = image2.ToBitmap();
+            }
+        }
+
+        
+        private void getResult(Image imgresult1, Image imgresult2)
+        {
+            //第五步：利用 哈希 計算 Hammingdistance 取得百分比
+
+            label1.Text = compareHash(GetHash(imgresult1), GetHash(imgresult2)).ToString() + "%";
+
+        }
+
+        private double compareHash(int[] first , int[] second)
+        {
+            //first
+            StringBuilder hashCode1 = new StringBuilder();
+            for (int i = 0; i < first.Length; i += 4)
+            {
+                int firstResult = first[i] * (int)Math.Pow(2, 3) + first[i + 1] * (int)Math.Pow(2, 2) + first[i + 2] * (int)Math.Pow(2, 1) + first[i + 2];
+                hashCode1.Append(string.Format("{0:x}", Convert.ToInt32(firstResult)));//二进制转为16进制  
+            }
+            String sourceHashCode1 = hashCode1.ToString();
+
+            //second
+            StringBuilder hashCode2 = new StringBuilder();
+            for (int i = 0; i < second.Length; i += 4)
+            {
+                int secondResult = second[i] * (int)Math.Pow(2, 3) + second[i + 1] * (int)Math.Pow(2, 2) + second[i + 2] * (int)Math.Pow(2, 1) + second[i + 2];
+                hashCode2.Append(string.Format("{0:x}", Convert.ToInt32(secondResult)));//二进制转为16进制  
+            }
+            String sourceHashCode2 = hashCode2.ToString();
+
+            //Hammingdistance
+            int difference = 0;
+            int length = sourceHashCode1.Length;
+
+            for (int i = 0; i < length; i++)
+            {
+                if (sourceHashCode1[i] != sourceHashCode2[i])
+                {
+                    difference++;
+                }
+            }
+
+            //0~16轉百分比
+            double result;
+            result = 100 - (difference * (100 / 16));
+
+            return result;
+        }
+
+        public int[] GetHash(Image imgresult1)
+        {
+            int width = 8;
+            int height = 8;
+
+            //第一步：縮小尺寸
+
+            Image SourceImg = ReduceSize(imgresult1, width, height);
+
+            //第二步：簡化色彩
+
+            Byte[] pixels = ReduceColor(SourceImg);
+
+            //第三步：計算平均
+
+            Byte average = CalcAverage(pixels);
+
+            //第四步：比較像素的灰度
+            int[] result = ComputeBits(pixels, average);
+
+
+            return result;
+        }
+
+        private Image ReduceSize(Image SourceImg, int width = 8, int height = 8)
+        {
+            Image image = SourceImg.GetThumbnailImage(width, height, () => { return false; }, IntPtr.Zero);
+            return image;
+        }
+
+        private Byte[] ReduceColor(Image image)
+        {
+            Bitmap bitMap = new Bitmap(image);
+            Byte[] grayValues = new Byte[image.Width * image.Height];
+
+            for (int x = 0; x < image.Width; x++)
+                for (int y = 0; y < image.Height; y++)
+                {
+                    Color color = bitMap.GetPixel(x, y);
+                    byte grayValue = (byte)((color.R * 30 + color.G * 59 + color.B * 11) / 100);
+                    grayValues[x * image.Width + y] = grayValue;
+                }
+            return grayValues;
+        }
+
+        private Byte CalcAverage(byte[] values)
+        {
+            int sum = 0;
+            for (int i = 0; i < values.Length; i++)
+                sum += (int)values[i];
+            return Convert.ToByte(sum / values.Length);
+        }
+
+        private int[] ComputeBits(byte[] values, byte averageValue)
+        {
+
+            int[] result = new int[values.Length];
+
+            for (int i = 0; i < values.Length; i++)
+            {
+                if (values[i] < averageValue)
+                    result[i] = 0;
+                else
+                    result[i] = 1;
+            }
+
+            return result;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            getResult(imgResult1.ToBitmap(), imgResult2.ToBitmap());
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            //預設70
+            double cannyValue = Convert.ToDouble(textBox1.Text);
+
+            //img 1 
+            Image<Gray, Byte> grayImage1 = imgResult1.Convert<Gray, Byte>();
+            Image<Gray, Byte> cannyGray1 = grayImage1.Canny(cannyValue, cannyValue);
+
+            imgCannyResult1 = cannyGray1;
+
+            //img 2 
+            Image<Gray, Byte> grayImage2 = imgResult2.Convert<Gray, Byte>();
+            Image<Gray, Byte> cannyGray2 = grayImage2.Canny(cannyValue, cannyValue);
+
+            imgCannyResult2 = cannyGray2;
+
+
+            getResult(imgCannyResult1.ToBitmap(), imgCannyResult2.ToBitmap());
+        }
+
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            label3.Text = "";
+
+            SqlConnection objCon;
+
+            objCon = new SqlConnection(strDbCon);
+
+            objCon.Open();
+
+            SqlCommand objcmd;
+            //-----------------------------------------------------------------------------
+            //create table
+
+            string TableColumn;
+
+            filename = new string[files.Length];
+            for (int i = 0; i < files.Length; i++)
+            {
+
+                TableColumn = "";
+
+                filename[i] = Path.GetFileNameWithoutExtension(files[i]);
+
+                TableColumn += filename[i] + " ( " + filename[i] + " char(128) );";
+
+                if (radioButton1.Checked)
+                {
+                    //新增資料表
+                    string strSQL1 = "CREATE TABLE " + TableColumn;
+
+                    objcmd = new SqlCommand(strSQL1, objCon);
+                    objcmd.ExecuteNonQuery();//執行可回傳次數
+
+                }
+
+                //掃圖
+                string[] imgs = Directory.GetFileSystemEntries(files[i]);
+
+                for (int j = 0; j < imgs.Length; j++)
+                {
+                    if (Path.GetExtension(imgs[j]) == ".jpg")
+                    {
+                        Image<Bgr, Byte> img;
+                        img = new Image<Bgr, Byte>(imgs[j]);
+
+                        int[] imgHash;
+                        imgHash = GetHash(img.ToBitmap());
+
+                        string value;
+                        value = "";
+
+                        for (int k = 0; k < imgHash.Length; k++)
+                        {
+
+                            value += imgHash[k] + ",";
+                            
+                        }
+                        //label3.Text += value;
+
+                        if (radioButton2.Checked)
+                        {
+                            //插入資料
+                            string strSQL2 = "INSERT INTO "+ filename[i] +" (" + filename[i] + ")VALUES ('" + value + "');";
+
+                            objcmd = new SqlCommand(strSQL2, objCon);
+                            objcmd.ExecuteNonQuery();//執行可回傳次數
+                        }
+
+                        //label3.Text += ",\n";
+                    }
+                }
+
+                //label3.Text += "\n";
+
+            }
+
+            if (radioButton1.Checked)
+            {
+                label3.Text = "新增成功";
+            }
+
+            if (radioButton2.Checked)
+            {
+                label3.Text = "更新成功";
+            }
+
+            if (radioButton3.Checked)
+            {
+
+                objCon.Close();
+
+                for (int i = 0; i < filename.Length; i++)
+                {
+
+                    objCon.Open();
+                    //讀取資料
+                    string strSQL3 = "select * from " + filename[i];
+
+                    objcmd = new SqlCommand(strSQL3, objCon);
+                    objcmd.ExecuteNonQuery();//執行可回傳次數
+
+                    //read SQLData
+                    SqlDataReader objDR = objcmd.ExecuteReader();
+
+                    while (objDR.Read())
+                    {
+
+                        for (int j = 0; j < objDR.FieldCount; j++)
+                        {
+
+                            label3.Text += String.Format(objDR[j].ToString()) + "\n";
+                        }
+
+                    }
+
+                    label3.Text += "\n";
+                    objCon.Close();
+                }
+
+                objCon.Open();
+
+            }
+
+            if (radioButton4.Checked)
+            {
+                objCon.Close();
+
+                for (int i = 0; i < filename.Length; i++)
+                {
+
+                    objCon.Open();
+
+                    //刪除資料表
+                    string strSQL4 = "DROP TABLE " + filename[i];
+
+                    objcmd = new SqlCommand(strSQL4, objCon);
+                    objcmd.ExecuteNonQuery();//執行可回傳次數
+                    
+                    objCon.Close();
+                }
+
+                objCon.Open();
+
+
+                label3.Text = "刪除成功";
+            }
+
+            //-----------------------------------------------------------------------------
+
+            objCon.Close();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            label3.Text = "";
+
+            int[] first = GetHash(imgResult1.ToBitmap());
+            
+            filename = new string[files.Length];
+            for (int i = 0; i < files.Length; i++)
+            {
+                filename[i] = Path.GetFileNameWithoutExtension(files[i]);
+
+            }
+            
+            SqlConnection objCon;
+            objCon = new SqlConnection(strDbCon);
+            
+            SqlCommand objcmd;
+
+            for (int i = 0; i < filename.Length; i++)
+            {
+
+                label3.Text += filename[i] + "\n";
+                double avgscore = 0.0;
+                int count = 0;
+
+                objCon.Open();
+                //讀取資料
+                string strSQL3 = "select * from " + filename[i];
+
+                objcmd = new SqlCommand(strSQL3, objCon);
+                objcmd.ExecuteNonQuery();//執行可回傳次數
+
+                //read SQLData
+                SqlDataReader objDR = objcmd.ExecuteReader();
+                while (objDR.Read())
+                {
+
+                    for (int j = 0; j < objDR.FieldCount; j++)
+                    {
+                        //將資料庫的檔案讀取後轉換為int array
+                        string[] s;
+                        s = objDR[j].ToString().Split(',');
+                        int[] second = new int[s.Length - 1];
+                        for (int k = 0; k < second.Length; k++)
+                        {
+                            second[k] = Convert.ToInt32(s[k]);
+                        }
+
+                        if (compareHash(first, second) >= 70)
+                        {
+                            avgscore += compareHash(first, second);
+                        }
+                        count++;
+
+                        //label3.Text += compareHash(first, second).ToString() + "%\n";
+
+                    }
+                }
+
+                //label3.Text += "\n";
+                label3.Text += (avgscore / count) + "\n";
+                objCon.Close();
+            }
+            
+        }
+    }
+}
